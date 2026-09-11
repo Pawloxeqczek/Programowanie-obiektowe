b690886be22703c8b333a4d8b37eb037060e9d1c
Paweł Chmiest <mlodyg@gmail.com>
2026-09-11 09:26:47 +0200

    Instrukcje wypisane

--- cwiczenia01/src/Main.java
+++ cwiczenia01/src/Main.java
@@ -1,13 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("witaj swiecie"); // wypisz i zrob enter
+    System.out.println("sout"); //skrot
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.print("cos bez robienia entera");
+    System.out.println("nowa linia");
+
+    System.out.println("w taki sposob robie znaki specjalne:");
+    System.out.println("\t tabulacja");
+    System.out.println("\n enter");
+    System.out.println("to jest jak na maszynie \rdo pisania");
+    System.out.println("\"cuzysluw\"");
 }
