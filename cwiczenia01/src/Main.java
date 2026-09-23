64feaf154bca515b10d54569176e57343d6c6317
Paweł Chmiest <mlodyg@gmail.com>
2026-09-23 13:41:51 +0200

    'zadanie02'

--- cwiczenia01/src/Main.java
+++ cwiczenia01/src/Main.java
@@ -10,4 +10,22 @@ void main() {
     System.out.println("\n enter");
     System.out.println("to jest jak na maszynie \rdo pisania");
     System.out.println("\"cuzysluw\"");
+    System.out.println("\'apostrof\'");
+    System.out.println("backslash: \\");
+
+    int liczbacalkowita = 5;
+    double liczbarzeczywista;
+    liczbarzeczywista = (double) liczbacalkowita/2;
+
+    System.out.println("wynik dzielenia 5 przez 2 to:" + liczbarzeczywista/2);
+
+    char znak = 'a';
+    System.out.println("Litera a ma wartosc " + (znak + 0));
+
+    String napis = "napis";
+    System.out.println("Napis wielkimi literami:" + napis.toUpperCase());
+
+    System.out.println("Wynik 1+2+3=" +1 +2 +3);
+    System.out.println("Wynik 1+2+3=" + (1 +2 +3) );
+    System.out.println( 1 + 2 + 3 + "to Wynik 1+2+3"  );
 }
